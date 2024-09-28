e015b9830c4120297c9b408a63bcb964cff29841
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-09-28 18:09:09 +0530

    working code

---
+++ led_brightness_with_2push_button_buzzer/src/main.cpp
@@ -0,0 +1,46 @@
+// C++ code
+//
+#include <Arduino.h>
+const int pin_read_increase= 7;
+const int pin_read_decrease= 2;
+const int led_pin=9;
+const int buzzer_pin=4;
+int led_brightness=0;
+
+void setup()
+{
+  pinMode(pin_read_increase, INPUT);
+  pinMode(pin_read_decrease, INPUT);
+  pinMode(led_pin, OUTPUT); 
+  pinMode(buzzer_pin, OUTPUT);   
+  Serial.begin(9600);
+}
+
+void loop()
+{
+int increase_button_state= digitalRead(pin_read_increase);
+int decrease_button_state= digitalRead(pin_read_decrease);
+Serial.print("brightness value "); 
+Serial.print(led_brightness); 
+Serial.println("");
+delay(250);
+  if (increase_button_state ==1){
+    led_brightness+=50;
+    if (led_brightness>255){
+      digitalWrite(buzzer_pin, HIGH);
+      delay(2400);
+      digitalWrite(buzzer_pin, LOW); 
+      led_brightness=255;
+    }
+  }
+  if (decrease_button_state ==1){
+    led_brightness-=50;
+      if (led_brightness<0){
+      digitalWrite(buzzer_pin, HIGH);      
+      delay(3000);
+      digitalWrite(buzzer_pin, LOW); 
+      led_brightness=0;
+    }
+  }   
+    analogWrite(led_pin,led_brightness);  
+}
