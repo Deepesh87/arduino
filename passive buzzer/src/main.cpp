b2a33d9af379d7c1b05ca86920ed73501f72cf30
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-09-29 20:11:05 +0530

    working code

---
+++ passive buzzer/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+// play random sounds that change
+
+int buzzPin= 8;
+int passive_buzz_tune(int buzzPin);
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(buzzPin, OUTPUT);
+}
+
+void loop() {
+passive_buzz_tune(buzzPin);
+}
+
+// put function definitions here:
+int passive_buzz_tune(int x) {
+tone(x,440);
+delay(500);
+tone(x,494);
+delay(500);
+tone(x,523);
+delay(500);
+tone(x,587);
+delay(500);
+tone(x,659);
+delay(500);
+tone(x,587);
+delay(500);
+tone(x,523);
+delay(500);
+tone(x,494);
+delay(500);
+tone(x,440);
+delay(500);
+}
