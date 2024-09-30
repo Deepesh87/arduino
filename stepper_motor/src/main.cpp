bd134fc9e85f5037ac6034a225567ee09247c4aa
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-09-30 18:14:04 +0530

    working code

---
+++ stepper_motor/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <Stepper.h>
+
+int stepsPerRevolution=2048;
+Stepper myStepper(stepsPerRevolution,8,9,10,11);
+int motSpeed=10; //rpm
+int dt=500;
+
+void setup() {
+Serial.begin(9600);
+myStepper.setSpeed(motSpeed);
+}
+
+void loop() {
+myStepper.step(stepsPerRevolution);
+delay(dt);
+myStepper.step(-stepsPerRevolution);
+delay(dt);
+}
+
