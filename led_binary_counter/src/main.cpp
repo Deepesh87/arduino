477902fa9b336e9247f365abd54b2c5c10058a32
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-09-30 13:26:00 +0530

    code with image

---
+++ led_binary_counter/src/main.cpp
@@ -0,0 +1,151 @@
+#include <Arduino.h>
+const int pinA=2;
+const int pinB=3;
+const int pinC=4;
+const int pinD=5;
+int delaytime=600;
+int pin_led_off(int x);
+int pin_led_on(int x);
+
+void setup() {
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  pinMode(pinC, OUTPUT);
+  pinMode(pinD, OUTPUT);
+}
+
+void loop() {
+pin_led_off(pinA);
+pin_led_off(pinB);
+pin_led_off(pinC);
+pin_led_off(pinD);
+delay(delaytime);
+// start=================
+pin_led_on(pinA);
+delay(delaytime);
+pin_led_off(pinA);
+delay(delaytime);
+pin_led_on(pinB);
+delay(delaytime);
+pin_led_off(pinB);
+delay(delaytime);
+//==============
+pin_led_on(pinA);
+pin_led_on(pinB);
+delay(delaytime);
+//===
+pin_led_off(pinA);
+pin_led_off(pinB);
+delay(delaytime);
+//==========
+pin_led_on(pinC);
+delay(delaytime);
+pin_led_off(pinC);
+delay(delaytime);
+//=======
+pin_led_on(pinC);
+pin_led_on(pinA);
+delay(delaytime);
+//=======
+pin_led_off(pinC);
+pin_led_off(pinA);
+delay(delaytime);
+//=======
+pin_led_on(pinC);
+pin_led_on(pinB);
+delay(delaytime);
+//=======
+pin_led_off(pinC);
+pin_led_off(pinB);
+delay(delaytime);
+//========
+pin_led_on(pinC);
+pin_led_on(pinB);
+pin_led_on(pinA);
+delay(delaytime);
+//=======
+pin_led_off(pinC);
+pin_led_off(pinB);
+pin_led_off(pinA);
+delay(delaytime);
+//========
+pin_led_on(pinD);
+delay(delaytime);
+//=======
+pin_led_off(pinD);
+delay(delaytime);
+//========
+pin_led_on(pinD);
+pin_led_on(pinA);
+delay(delaytime);
+//=======
+pin_led_off(pinD);
+pin_led_off(pinA);
+delay(delaytime);
+//========
+pin_led_on(pinD);
+pin_led_on(pinB);
+delay(delaytime);
+//=======
+pin_led_off(pinD);
+pin_led_off(pinB);
+delay(delaytime);
+//========
+pin_led_on(pinD);
+pin_led_on(pinB);
+pin_led_on(pinA);
+delay(delaytime);
+//=======
+pin_led_off(pinD);
+pin_led_off(pinB);
+pin_led_off(pinA);
+delay(delaytime);
+//========
+pin_led_on(pinD);
+pin_led_on(pinC);
+delay(delaytime);
+//=======
+pin_led_off(pinD);
+pin_led_off(pinC);
+delay(delaytime);
+//========
+pin_led_on(pinD);
+pin_led_on(pinC);
+pin_led_on(pinA);
+delay(delaytime);
+//=======
+pin_led_off(pinD);
+pin_led_off(pinC);
+pin_led_off(pinA);
+delay(delaytime);
+//========
+pin_led_on(pinD);
+pin_led_on(pinC);
+pin_led_on(pinB);
+delay(delaytime);
+//=======
+pin_led_off(pinD);
+pin_led_off(pinC);
+pin_led_off(pinB);
+delay(delaytime);
+//========
+pin_led_on(pinD);
+pin_led_on(pinC);
+pin_led_on(pinB);
+pin_led_on(pinA);
+delay(delaytime);
+//=======
+pin_led_off(pinD);
+pin_led_off(pinC);
+pin_led_off(pinB);
+pin_led_off(pinA);
+delay(delaytime);
+}
+
+int pin_led_off(int x){
+  digitalWrite(x, LOW);
+}
+
+int pin_led_on(int x){
+  digitalWrite(x, HIGH);
+}
