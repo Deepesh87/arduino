cec2c166a94b1d44273792c6ab8fc2c4f6efe355
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-10-02 22:30:27 +0530

    workig code

--- LCD/src/main.cpp
+++ LCD/src/main.cpp
@@ -1,22 +1,35 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-// vss goes to ground, vdd-5v, v0-middleof potentiometer, rw-gnd, 
-int rs=12;
-int en=11;
-int d4=5;
-int d5=4;
-int d6=3;
-int d7=2;
+/*vss goes to ground,
+vdd-5v,
+v0 - middleof potentiometer,
+rs- pin 7
+rw-gnd,
+E - 8,
+*/
+
+int rs=7;
+int en=8;
+int d4=9;
+int d5=10;
+int d6=11;
+int d7=12;
 
 LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
 
 void setup() {
 lcd.begin(16,2); // 16 cols and 2 rows
-lcd.print("Hello World!");
+
 }
 
 void loop() {
-lcd.setCursor(0,1);
-lcd.print(millis()/1000);
+lcd.setCursor(0,0);
+lcd.print("Watch me count!");
+for (int i=0; i<=100; i=i+1){
+    lcd.setCursor(0,1);
+    lcd.print(i);
+    delay(500);
+}
+lcd.clear();
 }
