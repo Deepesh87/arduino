0ec88e41457cd4dbf43b8f97470868ca6217c12e
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-10-06 00:12:17 +0530

    add comments

--- DHT11/src/main.cpp
+++ DHT11/src/main.cpp
@@ -3,11 +3,11 @@
 #include <Adafruit_Sensor.h> // dht has dependency on this
 // leftmost-- VCC, then Sensor pin, pin 3 not used, pin 4 is GND
 const int sensorPin= 7; 
-#define Type DHT11
+#define Type DHT11 //  to define the DHT sensor type you’re using
 float temp;
 float humidity;
 
-DHT dht(sensorPin, Type);
+DHT dht(sensorPin, Type); //initialize a DHT object with the pin and type
 
 void setup() {
 // pinMode(sensorPin, OUTPUT); not needed, already set in the library
