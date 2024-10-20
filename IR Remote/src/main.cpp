00a19179e11e31c3a7cd796a3fc808529879ab5a
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-10-20 12:20:39 +0530

    working code

--- IR Remote/src/main.cpp
+++ IR Remote/src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include <IRremote.h>
+/*
+program to view the HEX code for all buttons on a IR remote
 
+*/
 
 const int ir_pin=9;
-IRrecv IR(ir_pin);
-decode_results cmd;
+IRrecv IR(ir_pin); // create an IR object and give the pin number
+decode_results cmd; // command from library to read the results from the remote and save in a variable
 
 void setup() {
 Serial.begin(9600);
@@ -13,10 +16,11 @@ IR.blink13(true);
 }
 
 void loop() {
-  while(IR.decode(&cmd)==0){
+  while(IR.decode(&cmd)==0){   //call the value stored at this variable cmd using &
 }
 Serial.println(cmd.value, HEX);
 delay(1500);
 IR.resume();
 }
+
 
---
+++ Servo_motor_shield/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <AFMotor.h>
+
+/*
+do not install v2 library
+connect stepper motor to either m1 & 2 (port1)
+or M2 & M3(port2)
+NOTE: If powering the shield separately make sure to use the jumper cautiously.
+*/
+
+AF_Stepper my_stepmotor(200,1); //we connect to M1 on the shield
+int motSpeed=10; //rpm
+
+void setup() {
+  my_stepmotor.setSpeed(motSpeed);
+}
+
+void loop() {
+my_stepmotor.step(100, FORWARD, MICROSTEP);
+// my_stepmotor.step(250, BACKWARD, MICROSTEP);
+delay(1000);
+
+}
