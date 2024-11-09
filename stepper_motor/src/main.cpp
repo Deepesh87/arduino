9276fcd73e5882c30ae4451f5b03cab5c41eace8
Deepesh Singh <Deepesh87@users.noreply.github.com>
2024-11-09 18:56:01 +0530

    add purpose

--- stepper_motor/src/main.cpp
+++ stepper_motor/src/main.cpp
@@ -1,10 +1,17 @@
 #include <Arduino.h>
 #include <Stepper.h>
+/*
+learning to use stepper motor.
+A stepper motor needs a separate Power Source.  HW131 is what we will use here.
+We will use a driver as well for the Stepper motor. (see pics)
+to build the circuit. 
 
-int stepsPerRevolution=2048;
-// order is in1, 3, 2 and 4
+*/
+int stepsPerRevolution=2048; // 2048 steps make up a revolution.i.e. 360 degree
+// order is in1, 3, 2 and 4 (these are from the motor driver)
+// in1 - 8, in2 -9, in3-10, in4- 11
 Stepper myStepper(stepsPerRevolution,8,10,9,11);
-int motSpeed=10; //rpm
+int motSpeed=20; //rpm 2 to 20 tested.
 int dt=500;
 
 void setup() {
@@ -13,9 +20,9 @@ myStepper.setSpeed(motSpeed);
 }
 
 void loop() {
-myStepper.step(stepsPerRevolution);
+myStepper.step(stepsPerRevolution); // go full 360 degree, give stepsPerRevolution*2 for 2 rounds etc
 delay(dt);
-myStepper.step(-stepsPerRevolution);
+myStepper.step(-stepsPerRevolution); // go full 360 degree in opposite direction now.
 delay(dt);
 }
 
