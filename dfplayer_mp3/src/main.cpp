2e5fb8480d0de9d7c8e4a4032bd1ee45bde17061
deepesh87 <deepesh123123@gmail.com>
2025-01-11 14:36:17 +0530

    working code

--- dfplayer_mp3/src/main.cpp
+++ dfplayer_mp3/src/main.cpp
@@ -2,82 +2,68 @@
 #include <DFRobot_DF1201S.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySoftwareSerial(2, 3); //RX  TX
-DFRobot_DF1201S DF1201S;
-void play_first();
-void play_second();
 
+// Speakers must be connected to R+-or l+-. Code only works when uploaded rom Arduino IDe.
+//more details on DF player here https://wiki.dfrobot.com/DFPlayer_PRO_SKU_DFR0768#:~:text=Default%20serial%20baud%20rate%3A%20115200
+SoftwareSerial DF1201SSerial(2, 3);  //connect VIN to 5V, gnd to gnd and RX to 3  TX to 2
+ 
+DFRobot_DF1201S DF1201S; // create an object
+ 
+void setup(void) {
+  Serial.begin(115200);
+  DF1201SSerial.begin(115200);  // initialise the object
 
+  //return error if object is not found
+  while (!DF1201S.begin(DF1201SSerial)) {
+    Serial.println("Init failed, please check the wire connection!");
+    delay(1000);
+  }
 
-void setup()
-{
-Serial.begin(115200);
-mySoftwareSerial.begin(115200);
-// DF1201S.begin(mySoftwareSerial, 0);
-// while(!DF1201S.begin(mySoftwareSerial)){
-//   Serial.println("Init failed, please check the wire connection!");
-//   delay(1000);
-// }
-Serial.println("xxxxxxxxxxxxxxxx");
-DF1201S.setVol(30);
-Serial.print("VOL:");
-Serial.println(DF1201S.getVol());
-
-
-DF1201S.switchFunction(DF1201S.MUSIC);
-delay(2000);
-
-DF1201S.setPlayMode(DF1201S.SINGLE);
-Serial.print("PlayMode:");
-Serial.println(DF1201S.getPlayMode());
-DF1201S.setPrompt(false);
+  DF1201S.setVol(20); //set volumne to 20, 0 to 30 available.
+  Serial.print("VOL set is:");
+  Serial.println(DF1201S.getVol());
+  /*Enter music mode*/
+  DF1201S.switchFunction(DF1201S.MUSIC);
+  /*Wait for the end of the prompt tone */
+  /*Set playback mode to "SINGLE"*/
+  DF1201S.setPlayMode(DF1201S.SINGLE);
+  Serial.print("PlayMode:");
+  /*Get playback mode*/
+  Serial.println(DF1201S.getPlayMode());
+  Serial.print("The number of files available to play:");
+  //The number of files available to play
+  Serial.println(DF1201S.getTotalFile());
 }
-
-
+ 
 void loop() {
-Serial.println("==============================");
-// play_first(); 
-// Serial.println("abcdefghijklmnopqrstuvwxyz");
-// play_second();
-}
+/*
+i have saved 2 mp3 files one is dance_music.mp3 lasting around 90 seconds, another is GOT.mp3 lasting around 87 seconds
+FileNum for GOT is 1 and for dance_music is 2. (filenum is based on the order of saving the file on the player)
 
-void play_first() {
+*/
+  DF1201S.playFileNum(2); // will play file num 2
+  Serial.print("Name of file playing is:");
+  //Get the name of the playing file
+  Serial.println(DF1201S.getFileName());
+  Serial.print("Total duration of this file is : ");
+  Serial.println(DF1201S.getTotalTime()); // will give the total duration of the mp3 file in seconds
+  delay(10000);
   DF1201S.playFileNum(1);
-  DF1201S.setPlayTime(20);
-    Serial.print("Playing First Track... ");
-    Serial.println(DF1201S.getFileName());
-    Serial.println("Starts at 20 seconds in ");
-    Serial.println("Plays for 20 seconds ");
-    Serial.print("Track Length ");
-    Serial.println(DF1201S.getTotalTime());
-  delay(20000);
-    Serial.println("End of play_first()  ");
-    Serial.println(" ");    
+  Serial.print("Name of file playing is:");
+  //Get the name of the playing file
+  Serial.println(DF1201S.getFileName());
+  Serial.print("Total duration of this file is : ");
+  Serial.println(DF1201S.getTotalTime());
+
+  delay(10000);
+  Serial.println("moving to the next song now");  
+  DF1201S.next();
+  Serial.print("Name of file playing is:");
+  //Get the name of the playing file
+  Serial.println(DF1201S.getFileName());
+  delay(10000);  
+  Serial.println("The Loop ends here*******");
 }
 
-void play_second() {
-  DF1201S.playFileNum(02);
-  DF1201S.setPlayTime(30);
-    Serial.print("Playing Second Track... ");
-    Serial.println(DF1201S.getFileName());
-    Serial.println("Starting 30 seconds in ");
-  DF1201S.setVol(5);
-    Serial.print("VOL:");
-    Serial.println(DF1201S.getVol());
-    Serial.print("Track Length ");
-    Serial.println(DF1201S.getTotalTime());
-    Serial.println("Plays for 10 seconds ");
-    Serial.println(" ");
-  delay(10000);
-    Serial.println("Now skips to 40 seconds in ");
-  DF1201S.fastForward(/*FF = */40);
-    Serial.println("Plays for 10 seconds ");
-  delay(10000);
-    Serial.println(" ");
-  DF1201S.setPlayTime(30);
-    Serial.println("Skips to 30 seconds in ");
-    Serial.println("For 5 seconds ");
-    Serial.println("End of Instructions from play_second() ");
-  delay(5000);
-    Serial.println(" ");
-}
+
+// DF1201S.setPrompt(false); // to stop the prompt music music 
