51f519dcaea40c2272d4d2fae4a15e35b5506d36
deepesh87 <deepesh123123@gmail.com>
2025-02-03 23:58:37 +0530

    deployed

--- 1_project_led_balance_bike/src/main.cpp
+++ 1_project_led_balance_bike/src/main.cpp
@@ -5,18 +5,24 @@ project to have a few RBG leds on a balance bike and User would control them usi
 
 // define the pins for the RGB leds
 //first led
-byte redPin = 3;
-byte greenPin = 4;
-byte bluePin = 5;
+byte redPin = 2;
+byte greenPin = 3;
+byte bluePin = 4;
 //second led
-byte redPin2 = 8;
-byte greenPin2 = 9;
-byte bluePin2 = 10;
+byte redPin2 = 6;
+byte greenPin2 = 7;
+byte bluePin2 = 8;
+//third led
+byte redPin3 = 11;
+byte greenPin3 = 12;
+byte bluePin3 = 13;
 //pins for the push button. Will use a pull down resistor
-byte buttonPin = 13;
+byte buttonPin = A0;
 // state 1 is RED, state 2 is BLUE, state 3 is GREEN. will start with Red
 int state = 1;
 int pushButtonState = 0;
+// time from the begining of the loop
+int time = 0;
 
 void setup() {
   pinMode(redPin, OUTPUT);
@@ -25,17 +31,26 @@ void setup() {
   pinMode(redPin2, OUTPUT);
   pinMode(greenPin2, OUTPUT);
   pinMode(bluePin2, OUTPUT);
+  pinMode(redPin3, OUTPUT);
+  pinMode(greenPin3, OUTPUT);
+  pinMode(bluePin3, OUTPUT);
+
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
+  time = millis();
+  // after 20 seconds, do nothing
+  if (time > 20000) { 
+    state = 0;//do nothing since we dont have a case for state 0 below
+  }
   // read the state of the push button
   pushButtonState = digitalRead(buttonPin);
   delay(200);
   // if the button is pressed, change the state of the leds
   if (pushButtonState == HIGH) {
     state++;
-    if (state > 3) {
+    if (state > 7) {
       state = 1;
     }
   }
@@ -50,6 +65,10 @@ void loop() {
       digitalWrite(redPin2, HIGH);
       digitalWrite(greenPin2, LOW);
       digitalWrite(bluePin2, LOW);
+      //turn on the third led RED
+      digitalWrite(redPin3, HIGH);
+      digitalWrite(greenPin3, LOW);
+      digitalWrite(bluePin3, LOW);
       break;
     case 2:
       //turn on the first led BLUE
@@ -60,6 +79,10 @@ void loop() {
       digitalWrite(redPin2, LOW);
       digitalWrite(greenPin2, LOW);
       digitalWrite(bluePin2, HIGH);
+      //turn on the third led BLUE
+      digitalWrite(redPin3, LOW);
+      digitalWrite(greenPin3, LOW);
+      digitalWrite(bluePin3, HIGH);
       break;
     case 3:
       //turn on the first led GREEN
@@ -70,6 +93,58 @@ void loop() {
       digitalWrite(redPin2, LOW);
       digitalWrite(greenPin2, HIGH);
       digitalWrite(bluePin2, LOW);
+      //turn on the third led GREEN
+      digitalWrite(redPin3, LOW);
+      digitalWrite(greenPin3, HIGH);
+      digitalWrite(bluePin3, LOW);
+      break;
+    case 4:
+      //do a colorful chase
+      digitalWrite(redPin, HIGH);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(bluePin, LOW);
+      digitalWrite(redPin2, LOW);
+      digitalWrite(greenPin2, HIGH);
+      digitalWrite(bluePin2, LOW);
+      digitalWrite(redPin3, LOW);
+      digitalWrite(greenPin3, LOW);
+      digitalWrite(bluePin3, HIGH); 
+      break;
+    case 5:
+      //do a colorful chase 2
+      digitalWrite(redPin, LOW);
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(bluePin, LOW);
+      digitalWrite(redPin2, HIGH);
+      digitalWrite(greenPin2, LOW);
+      digitalWrite(bluePin2, LOW);
+      digitalWrite(redPin3, LOW);
+      digitalWrite(greenPin3, LOW);
+      digitalWrite(bluePin3, HIGH); 
+      break;
+    case 6:
+      //do a colorful chase 3
+      digitalWrite(redPin, LOW);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(bluePin, HIGH);
+      digitalWrite(redPin2, HIGH);
+      digitalWrite(greenPin2, LOW);
+      digitalWrite(bluePin2, LOW);
+      digitalWrite(redPin3, LOW);
+      digitalWrite(greenPin3, HIGH);
+      digitalWrite(bluePin3, LOW); 
+      break;
+    case 7:
+      //turn off all the leds
+      digitalWrite(redPin, LOW);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(bluePin, LOW);
+      digitalWrite(redPin2, LOW);
+      digitalWrite(greenPin2, LOW);
+      digitalWrite(bluePin2, LOW);
+      digitalWrite(redPin3, LOW);
+      digitalWrite(greenPin3, LOW);
+      digitalWrite(bluePin3, LOW);
       break;
   }
 }
