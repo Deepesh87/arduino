cddcfa92360572aa94c076fbc91a24cc8123c94c
deepesh87 <deepesh123123@gmail.com>
2025-04-17 23:09:22 +0530

    add library name

--- 1_IR_remote_all_buttons/src/main.cpp
+++ 1_IR_remote_all_buttons/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <IRremote.h>
 /*
+Library is IRremote by Shirriff--
 program to map the HEX code for all buttons on a IR remote
 using the latest version of library. so these functions may not be on most youtube videos
 IR sensor connection- when S is written on the right..
